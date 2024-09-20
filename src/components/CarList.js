610bdcc058e5ded62be16f24ce13fb4872f79bc3
redouane <redouane.hitachi@gmail.com>
2024-09-20 11:00:28 -0700

    cars project _redux_

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -2,15 +2,21 @@ import { useDispatch, useSelector } from "react-redux"
 import { removeCar } from "../store";
 export default function CarList() {
     const dispatch = useDispatch();
-    const cars = useSelector(({ cars: { data, searchTerm } }) => {
-        return data.filter((car) => car.name.toLowerCase().includes(searchTerm.toLowerCase()));
+    const { cars, name } = useSelector(({ form, cars: { data, searchTerm } }) => {
+        const filteredCars = data.filter((car) => car.name.toLowerCase().includes(searchTerm.toLowerCase()));
+        return {
+            cars: filteredCars,
+            name: form.name
+        }
     });
+
     const handleCarDelete = (car) => {
         dispatch(removeCar(car.id))
     }
     const renderCars = cars.map((car) => {
+        const bold = name && car.name.toLowerCase().includes(name.toLowerCase())
         return (
-            <div key={car.id} className="panel">
+            <div key={car.id} className={`panel ${bold && 'bold'}`}>
                 <p>
                     {car.name}- ${car.cost}
                 </p>
--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import { useSelector } from "react-redux"
+
 
 export default function CarValue() {
+    const totalCost = useSelector(({ cars: { data, searchTerm } }) => {
+        const filteredCars = data.filter((car) => {
+            return car.name.toLowerCase().includes(searchTerm.toLowerCase())
+        });
+        return filteredCars.reduce((acc, car) => {
+            return acc + car.cost;
+        }, 0)
+    })
     return (
-        <div>
-            CarValue
+        <div className='car-value'>
+            Total cost:${totalCost}
         </div>
     )
 }
